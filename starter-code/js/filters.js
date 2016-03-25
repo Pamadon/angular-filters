c86ba5bf3a383dd2892acbd25f475050bb67dd75
bhague1281 <bhague1281@gmail.com>
2016-03-25 14:22:14 -0700

    filters example

--- starter-code/js/filters.js
+++ starter-code/js/filters.js
@@ -1,10 +1,76 @@
 angular.module('myFilters', [])
 
-// here's a sample filter
-.filter('dummyfilter', function() {
+.filter('capitalize', function() {
+  var capitalizeWord = function(word) {
+    return word[0].toUpperCase() + word.substring(1, word.length).toLowerCase();
+  }
+
   return function(input) {
-    return 'Dummy Text!';
-  };
+    if (!input) return input;
+    input = input.toString();
+    return input.split(' ').map(capitalizeWord).join(' ');
+  }
 })
 
-// define your own here. this module will be injected in app.js
+.filter('ordinal', function() {
+  return function(input) {
+    if (isNaN(input)) return input;
+
+    var secondToLastNumber = parseInt(input / 10) % 10;
+    var lastNumber = parseInt(input) % 10;
+    var ordinal = 'th';
+    if (secondToLastNumber != 1) {
+      switch(lastNumber) {
+        case 1:
+          ordinal = 'st';
+          break;
+        case 2:
+          ordinal = 'nd';
+          break;
+        case 3:
+          ordinal = 'rd';
+          break;
+      }
+    }
+    return input + ordinal;
+  }
+})
+
+.filter('swearFilter', function() {
+  var filteredWords = ['homework', 'swagger'];
+
+  return function(input, setting) {
+    var swearWord = filteredWords.indexOf(input) > -1
+    if (swearWord) {
+      var filtered = '';
+
+      if (setting === "PG13") {
+        var starting = 1;
+        filtered = input[0];
+      } else {
+        var starting = 0;
+      }
+
+      for (var i = starting; i < input.length; i++) {
+        filtered += '*';
+      }
+
+      return filtered;
+    } else {
+      return input;
+    }
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
